7466c4a627a1473026d755aca8cf860fa3398bdf
Joel Vandiver <joelvandiver@hotmail.com>
2018-12-03 08:55:57 -0500

    beginning HTML DSL

--- src/SiteGen.Lib/Library.fs
+++ src/SiteGen.Lib/Library.fs
@@ -1,5 +1,29 @@
 namespace SiteGen.Lib
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+module Site =
+    type Inline =
+    | Text of string
+    | P of Inline
+    | Span of Inline
+    | Strong of Inline
+    | Anchor of href:string * Inline
+    | Script of src:string
+
+    type Block =
+    | Div of Block
+    | Ul of Block list
+    | Ol of Block list
+
+    type Header =
+    | Link of href:string * rel:string
+    | MetaCharset of string
+    | MetaName of name:string * content:string
+    | MetaHttp of httpEquiv:string * content:string
+    | Title of string
+
+    type Html = 
+        {   Head: Header
+            Body: Block }
+
+    let print (html: Html) : string = 
+        ""
--- test/SiteGen.Lib.Tests/Program.fs
+++ test/SiteGen.Lib.Tests/Program.fs
@@ -2,13 +2,23 @@
 open Expecto
 open SiteGen.Lib
 
-let say = Say.hello "Joel"
-
 let tests =
   test "A simple test" {
     let subject = "Hello World"
     Expect.equal subject "Hello World" "The strings should equal"
   }
+
+
+// should create an <html> tag.
+// should create an html document.
+// should create an <body>.
+// should create a <p> tag.
+// should create a <strong> tag.
+// should create a <div> tag.
+// should create a <span> tag.
+// should create a <li> tag.
+// should create a <ul> tag.
+// should create a <ol> tag.
 
 [<EntryPoint>]
 let main args = runTestsWithArgs defaultConfig args tests

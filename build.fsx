1498cffa805fcfb82ecf98e920ce11566c54530a
Joel Vandiver <joelvandiver@hotmail.com>
2018-12-08 17:04:57 -0500

    deprecate Site.Gen.fsproj

--- build.fsx
+++ build.fsx
@@ -7,32 +7,13 @@ open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
 open Fake.DotNet.Testing
 
-Target.create "Clean" (fun _ ->
-    !! "src/**/bin"
-    ++ "src/**/obj"
-    ++ "test/**/obj"
-    ++ "test/**/obj"
-    |> Shell.cleanDirs 
-)
-
 Target.create "Build" (fun _ ->
-    !! "src/**/*.fsproj"
-    ++ "test/**/*.fsproj"
-    |> Seq.iter (DotNet.build id)
-)
-
-Target.create "Test" (fun _ -> 
-    let setParams ps = ps
-    let assemblies = 
-        !! "test/**/Debug/**/*.Tests.dll"
-    Expecto.run setParams assemblies
+    printfn "Building..."
 )
 
 Target.create "All" ignore
 
-"Clean"
-  ==> "Build"
-  ==> "Test"
+"Build"
   ==> "All"
 
 Target.runOrDefault "All"

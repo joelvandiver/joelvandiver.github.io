9f99153339e5d1e91e8c0dce7fa917ce8a986158
Joel Vandiver <joelvandiver@hotmail.com>
2018-12-08 14:24:20 -0500

    Math Explorations

---
+++ posts/Math/Functions/Domain-and-Range.fsx
@@ -0,0 +1,5 @@
+let S = [(1, 4); (2, 3); (2, 2); (4, 3); (5, 4)] |> Set.ofList
+let Domain S = S |> Set.map(fun (x, _) -> x)
+let Range S = S |> Set.map(fun (_, y) -> y)
+let DomainS = S |> Domain
+let RangeS = S |> Range

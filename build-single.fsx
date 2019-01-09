3722ef2fe4561dd43389ae6e70a0494dc2d1b9b7
Joel Vandiver <joelvandiver@hotmail.com>
2019-01-09 08:30:19 -0500

    // TODO:  Build Single with Current File through VSCode Task

--- build-single.fsx
+++ build-single.fsx
@@ -6,9 +6,21 @@ open FSharp.Literate
 
 // TODO:  Build Single with Current File through VSCode Task
 
+let args = 
+   fsi.CommandLineArgs 
+   |> List.ofArray
+
+let file = 
+   let found = 
+      args
+      |> List.tryFind(fun arg -> arg.StartsWith("file:"))
+   match found with 
+   | None -> raise (System.Exception("The `file:` command line argument is required."))
+   | Some file -> file.Replace("file:", "")
+
+printfn "Running Build for File:  %s" file
+
 let source = __SOURCE_DIRECTORY__
 let template = Path.Combine(__SOURCE_DIRECTORY__, @"content/template.html")
-let file = @"C:\git\joelvandiver.github.io\posts\tutorials\fs\fsx\cli-args\index.fsx"
-// let file = @"C:\git\joelvandiver.github.io\posts\explorations\interview-questions\column-header-number\index.fsx"
 
 Literate.ProcessScriptFile(file, template)

74c4406391bbf38a22b3b87c25d0f6c718efdd82
Joel Vandiver <joelvandiver@hotmail.com>
2019-01-15 08:55:39 -0500

    Convert Spreadsheet Column Headers to a Number

---
+++ TODO.md
@@ -0,0 +1,5 @@
+# TODO
+
+- Colum-Header-Number\index.fsx
+  - TODO:  Convert a whole number to a spreadsheet column header.
+
--- posts/Explorations/Interview-Questions/Colum-Header-Number/index.fsx
+++ posts/Explorations/Interview-Questions/Colum-Header-Number/index.fsx
@@ -1,37 +1,66 @@
 (**
 # Convert Spreadsheet Column Headers to a Number
 
-## Problem 1:  Convert a whole number to a spreadsheet column header.
-
 ### Expected Sample Set:
 | x | y |
 | -- | -- |
-| 1 | A |
-| 2 | B |
-| 3 | C |
-| 4 | D |
+| A | 1 | 
+| B | 2 | 
+| C | 3 | 
+| D | 4 | 
 | ... | ... |
-| 27 | AA |
-| 28 | AB |
-| 29 | AC |
-| 30 | AD |
-| 31 | AE |
+| AA | 27 | 
+| AB | 28 | 
+| AC | 29 | 
+| AD | 30 | 
+| AE | 31 | 
 | ... | ... |
 
 *)
 
 open System
 
-let alpha = 
-   "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+let convertText (text: string) : Double =
+   text
    |> List.ofSeq
+   |> List.rev
+   |> List.mapi(
+         fun i a -> 
+            let power27 = Math.Pow(27., (float i))
+            let alphaIndex = alpha.IndexOf(a) + 1 |> float
+            let result = power27 * alphaIndex
+            result)
+   |> List.reduce(+)
 
-let divide (q: double) (d: double) : double * double =
-   Math.Truncate(q / d), q % d
+(**
+*Output:*
+```console
+val alpha : string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+val convertText : text:string -> Double
+```
+*)
 
-// let convertBase (n: double) (b: double) : 
+(**
+### Test
+*)
 
-let getColumnName (n: double) : string =
-   match n with 
-   | n when n <= 0 ->  ""
-   | _ -> ""
+let data = ["AA"; "AB"; "AC"; "AD"; "AE"; "AF"; "AG"; "AH"; "AI"; "AJ";]
+let expected = [28; 29; 30; 31; 32; 33; 34; 35; 36; 37;] |> List.map float
+
+let result = data |> List.map convertText
+let test = expected = result
+
+(**
+*Output:*
+```console
+val data : string list =
+  ["AA"; "AB"; "AC"; "AD"; "AE"; "AF"; "AG"; "AH"; "AI"; "AJ"]
+val expected : float list =
+  [28.0; 29.0; 30.0; 31.0; 32.0; 33.0; 34.0; 35.0; 36.0; 37.0]
+val result : Double list =
+  [28.0; 29.0; 30.0; 31.0; 32.0; 33.0; 34.0; 35.0; 36.0; 37.0]
+val test : bool = true
+```
+*)

a661658ae5848bbf490626b5be68764b310d0544
Joel Vandiver <joelvandiver@hotmail.com>
2019-06-26 08:22:07 -0400

    Implement a React Hook

--- posts/Guides/Fable/react/src/App.fs
+++ posts/Guides/Fable/react/src/App.fs
@@ -3,24 +3,28 @@ module App
 open Fable.Core
 open Fable.Core.JsInterop
 open Fable.Import
-// open Fable.Import.Browser
 open Browser.Types
 open Browser.Dom
 open Fable.React
 open Fable.React.Props
 open Fable.Core.Util
-// open Fable.Import.React
-// open Fable.Helpers.React
-// open Fable.Helpers.React.Props
 open Fable.Core.Extensions
 
 // Ref:  https://blog.vbfox.net/2018/02/06/fable-react-1-react-in-fable-land.html
 
+module ReactHookExample =
+    let view =
+        FunctionComponent.Of(fun (props: {| initCount: int |}) ->
+        let state = Hooks.useState(props.initCount) // This is where the magic happens
+        button
+            [ OnClick (fun _ -> state.update(fun s -> s + 1)) ]
+            [ str "Times clicked: "; ofInt state.current ]
+        )
+
 module StatelessExample = 
     let welcome =    
         FunctionComponent.Of(fun (props: {| message: string |}) ->
-            div [] [str props.message])
-
+            span [] [str props.message])
 
 module StateExample = 
     // A pure, stateless component that will simply display the counter
@@ -29,7 +33,7 @@ module StateExample =
     type CounterDisplay(initialProps) =
         inherit PureStatelessComponent<CounterDisplayProps>(initialProps)
         override this.render() =
-            div [] [ str "Counter = "; ofInt this.props.counter ]
+            span [] [ str "Counter = "; ofInt this.props.counter ]
 
     let inline counterDisplay p = ofType<CounterDisplay,_,_> p []
 
@@ -39,7 +43,7 @@ module StateExample =
     type AddRemove(initialProps) =
         inherit PureStatelessComponent<AddRemoveProps>(initialProps)
         override this.render() =
-            div [] [
+            span [] [
                 button [OnClick this.props.add] [str "👍"]
                 button [OnClick this.props.remove] [str "👎"]
             ]
@@ -66,57 +70,42 @@ module StateExample =
             this.setState(fun counterState -> fun t -> { counter = counterState.counter - 1 })
 
         override this.render() =
-            div [] [
+            span [] [
                 counterDisplay { CounterDisplayProps.counter = this.state.counter }
                 addRemove { add = add; remove = remove }
             ]
 
     let inline counter props = ofType<Counter,_,_> props []
 
-
-
-
 let init() =
     let element =
-        // Each HTML element has an helper with the same name
         ul
-            // The first parameter is the properties of the elements.
-            // For html elements they are specified as a list and for custom
-            // elements it's more typical to find a record creation
             [ClassName "my-ul"; Id "unique-ul"]
-
-            // The second parameter is the list of children
             [
-                li [] [ StatelessExample.welcome {| message = "🌍" |} ]
-                li [] [ StateExample.counter createEmpty ]
-                // str is the helper for exposing a string to React as an element
-                li [] [ str "Hello 🌍" ]
+                li [] [ str "React Hook Example -> "; ReactHookExample.view {| initCount = 0 |} ]                
+                li [] [ str "Stateless Example -> "; StatelessExample.welcome {| message = "🌍" |} ]
+                li [] [ str "State Example -> "; StateExample.counter createEmpty ]
+                li [] [ str "String Example -> "; str "Hello 🌍" ]
+                li [] [ str "ofInt Example -> "; str "The answer is: "; ofInt 42 ]
+                li [] [ str "ofFloat Example -> "; str "π="; ofFloat 3.14 ]
+                li [] [ str "ofOption (null or something) Example -> "; str "🤐"; ofOption (Some (str "🔫")) ]
+                li [] [ str "ofOption (render nothing) Example -> "; str "😃"; ofOption None ]
+                li [] [ str "ofList Example -> "
+                        ul []
+                            [ [1;2;3]
+                                |> List.map(fun i ->
+                                    let si = i.ToString()
+                                    li [Key si] [str "🎯 "; str si])
+                                |> ofList ] ]
 
-                // Helpers exists also for other primitive types
-                li [] [ str "The answer is: "; ofInt 42 ]
-                li [] [ str "π="; ofFloat 3.14 ]
-
-                // ofOption can be used to return either null or something
-                li [] [ str "🤐"; ofOption (Some (str "🔫")) ]
-                // And it can also be used to unconditionally return null, rendering nothing
-                li [] [ str "😃"; ofOption None ]
-
-                // ofList allows to expose a list to react, as with any list of elements
-                // in React each need an unique and stable key
-                [1;2;3]
-                    |> List.map(fun i ->
-                        let si = i.ToString()
-                        li [Key si] [str "🎯 "; str si])
-                    |> ofList
-
-                // fragment is the <Fragment/> element introduced in React 16 to return
-                // multiple elements
-                [1;2;3]
-                    |> List.map(fun i ->
-                        let si = i.ToString()
-                        li [] [str "🎲 "; str si])
-                    |> fragment []
-            ]
+                li [] 
+                    [ str "React Fragment Example -> "
+                      ul []
+                        [ [1;2;3]
+                            |> List.map(fun i ->
+                                let si = i.ToString()
+                                li [] [str "🎲 "; str si])
+                            |> fragment [] ] ] ]
     ReactDom.render(element, document.getElementById("root"))
 
 init()

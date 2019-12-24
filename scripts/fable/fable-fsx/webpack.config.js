b8406c54fcdac3e8a5647227a0e1d1e5461054ba
Joel Vandiver <joelvandiver@gmail.com>
2019-12-24 16:57:01 -0600

    2019-12-24-fable-fsx.md

--- scripts/fable/fable-fsx/webpack.config.js
+++ scripts/fable/fable-fsx/webpack.config.js
@@ -1,15 +1,11 @@
-// Note this only includes basic configuration for development mode.
-// For a more comprehensive configuration check:
-// https://github.com/fable-compiler/webpack-config-template
-
 var path = require("path");
 
 module.exports = {
     mode: "development",
-    entry: "./posts/Daily/2019/04/07/Fable-FSX/index.fsx",
+    entry: "./fable.fsx",
     output: {
         path: path.join(__dirname, "./"),
-        filename: "bundle.js",
+        filename: "fable-fsx.js",
     },
     devServer: {
         contentBase: "./",
